37860bd4df4501adf1ef1eefdbe28f7cb9673a36
Silasoli <silas.oliveira@blockhub.builders>
2022-11-08 19:34:20 -0300

    fix: removendo rota padrão

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import 'dotenv/config';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.DATABASE_URI), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
